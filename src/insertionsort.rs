722b40cbab506a0a421e3c21703ca3c54745c6cd
Karan V <karanvkanarayanan@gmail.com>
2022-08-11 09:56:36 +0530

    remove comments

--- src/insertionsort.rs
+++ src/insertionsort.rs
@@ -9,14 +9,7 @@ impl<T> Sorter<T> for InsertionSort {
     where
         T: Ord,
     {
-        // [ sorted | not sorted ]
         for unsorted in 1..slice.len() {
-            // slice[unsorted..] is not sorted
-            // take slice[unsorted] and place in sorted location in slice[..=unsorted]
-            // [ 1 3 4 | 2 ]
-            // [ 1 3 4 2 | ]
-            // [ 1 3 2 4 | ]
-            // [ 1 2 3 4 | ]
             if !self.smart {
                 let mut i = unsorted;
                 while i > 0 && slice[i - 1] > slice[i] {
@@ -24,12 +17,9 @@ impl<T> Sorter<T> for InsertionSort {
                     i -= 1;
                 }
             } else {
-                // use binary search to find index
-                // then use .insert to splice in i
                 let i = match slice[..unsorted].binary_search(&slice[unsorted]) {
-                    // [ a, c, e].binary_search(c) => Ok(1)
                     Ok(i) => i,
-                    // [ a, c, e].binary_search(b) => Err(1)
+
                     Err(i) => i,
                 };
                 slice[i..=unsorted].rotate_right(1);
